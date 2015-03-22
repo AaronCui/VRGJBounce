32d4b06575c13e5e227a7d2b2efedb2a7fe6e50b
g18-kc <kerbychang@gmail.com>
2015-03-21 18:50:45 -0700

    update scripts

--- Assets/textController.cs
+++ Assets/textController.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class textController : MonoBehaviour {
 
+	private float dt;
+	private string tmp;
 
 	// Use this for initialization
 	void Start () {
-	
+		
+		dt = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		textChange ("hello world");
+		dt += Time.deltaTime;
+		tmp = "";
+		tmp += dt;
+		textChange (tmp);
 	}
 
 	public void textChange(string str){

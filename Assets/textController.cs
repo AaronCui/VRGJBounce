568aad91142a4b4ece24deed60b3ddf6718ddfdd
g18-kc <kerbychang@gmail.com>
2015-03-21 18:48:06 -0700

    guiText

---
+++ Assets/textController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textChange ("hello world");
+	}
+
+	public void textChange(string str){
+
+		gameObject.GetComponent<TextMesh> ().text = str;
+	}
+
+}
